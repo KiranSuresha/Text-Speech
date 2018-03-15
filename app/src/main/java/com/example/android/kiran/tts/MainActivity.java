d8b02ad60ab2e31f3b36378125e6ec700e0533e0
Kiran Suresha <kiran.s.2528@gmail.com>
2018-03-15 05:09:13 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/kiran/tts/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.android.kiran.tts;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private Button btnSpeak;
+    private TextToSpeech textToSpeech;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        textToSpeech = new TextToSpeech(this, this);
+
+        btnSpeak = findViewById(R.id.btnSpeak);
+
+        editText = findViewById(R.id.txtText);
+
+        // button on click event
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                speakOut();
+            }
+
+        });
+    }
+
+    @Override
+    public void onInit(int i) {
+
+        if (i == TextToSpeech.SUCCESS) {
+
+            int result = textToSpeech.setLanguage(Locale.US);
+
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "This Language is not supported");
+            } else {
+                btnSpeak.setEnabled(true);
+                speakOut();
+            }
+
+        } else {
+            Log.e("TTS", "Initialization Failed!");
+        }
+    }
+
+    private void speakOut() {
+
+        String text = editText.getText().toString();
+
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        textToSpeech.setPitch(0.6f);
+        textToSpeech.setSpeechRate(2);
+    }
+
+    @Override
+    public void onDestroy() {
+        // Don't forget to shutdown tts!
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+}
